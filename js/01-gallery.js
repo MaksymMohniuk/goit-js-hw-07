528c9312da67eaf4cd0175df027374eff25385e5
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2023-01-22 23:18:13 +0200

    finish-task-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,12 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(createGalleryMarkup(galleryItems));
 
 function createGalleryMarkup(galleryItems) {
-    const markup = galleryItems.map(({preview, original, description}) => {
-    return `<div class="gallery__item">
+  const markup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
     <a class="gallery__link" href="${original}">
       <img
         class="gallery__image"
@@ -14,35 +15,33 @@ function createGalleryMarkup(galleryItems) {
         alt="${description}"
       />
     </a>
-  </div>` }).join(''); 
+  </div>`;
+    })
+    .join("");
   return markup;
 }
 
-const galleryMarkupContainer = document.querySelector('.gallery');
+const galleryMarkupContainer = document.querySelector(".gallery");
 
 const galleryMarkup = createGalleryMarkup(galleryItems);
 
-galleryMarkupContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryMarkupContainer.insertAdjacentHTML("beforeend", galleryMarkup);
 
-galleryMarkupContainer.addEventListener('click', returnUrlOfBiggestImg);
+galleryMarkupContainer.addEventListener("click", returnUrlOfBiggestImg);
 
 function returnUrlOfBiggestImg(event) {
   event.preventDefault();
-    if (event.target.nodeName !== 'IMG') {
-        return
-     }
-const bigImg = basicLightbox.create(`
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const bigImg = basicLightbox.create(`
      <img src="${event.target.dataset.source}" width="800" height="600">
-  `)
+  `);
   bigImg.show();
-  
-  galleryMarkupContainer.addEventListener('keydown', (event) => {
-    if(event.code === 'Escape') {
+
+  galleryMarkupContainer.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
       bigImg.close();
     }
-    });
+  });
 }
-
-
-
-
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+function createGalleryMarkup(galleryItems) {
+  const markup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`;
+    })
+    .join("");
+  return markup;
+}
+const galleryMarkupContainer = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryMarkupContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
